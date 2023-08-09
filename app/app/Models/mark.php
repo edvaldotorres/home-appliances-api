32896697b4b9b3c443d678e78555f80150de9528
edvaldotorres <edvaldo.torres@tisaude.com>
2023-08-09 19:52:50 -0300

    fix: name models and migrate

--- app/app/Models/mark.php
+++ app/app/Models/mark.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class mark extends Model
+class Mark extends Model
 {
     use HasFactory, SoftDeletes;
 
